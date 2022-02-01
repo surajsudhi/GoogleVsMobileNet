fc5162553d6b5ff3ecdf998a592e7d371963fa9f
surajsudhi <suraj.sudhi@gmail.com>
2022-02-01 19:01:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-
+var Bed ;
+function setup() {
+    classfier= ml5.imageClassifier('MobileNet', modelLoaded)
+  }
+  function draw(){
+    }
+    function modelLoaded(){
+    console.log("ModelLoaded");
+    classfier.classify(Bed, gotResult)  
+    }
+   function preload(){
+  Bed = loadImage("Door knob.jpg")
+   }
+    function gotResult(error, result){
+        if(error){
+        console.error(error);  
+        } 
+        else{
+         console.log(result);
+        }
+       }
 
